43c175dc7621895877a830864d4ce0e0434efa10
kumatata <coumba.camara75@gmail.com>
2020-06-02 18:32:10 +0200

    exo 8 n est pas terminé

--- routes/appExpress.js
+++ routes/appExpress.js
@@ -1,16 +1,30 @@
 var express = require('express');
 var http = require('http');
 var app = express();
+var bodyParser = require('body-parser');
 
 app.use((req,res,next) => {
     console.log(req.protocol + '://' + req.get('host') + req.originalUrl) ;
     next();
       })
+     
+app.set('view engine', 'pug');
+app.set('views', './views');
+     
+ // for parsing application/json
+app.use(bodyParser.json()); 
+     
+// for parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: true })); 
+//form-urlencoded
+     
+app.post('/', function(req, res){
+    console.log(req.body);
+    res.send("Success");
+     });
 
 app.get('/', function(req, res){
-    res.writeHead(200);
-    res.write('Hello Express!!!');
-    res.end();});
+    res.render('form');});
     http.createServer(app).listen(3000);
 
 app.get('/salut', function(req,res){
