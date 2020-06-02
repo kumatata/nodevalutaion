fbe815c6443045aed0fbf56aabdc3e3dc4d472a8
kumatata <coumba.camara75@gmail.com>
2020-06-02 17:21:56 +0200

    exo3

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-var http = require('http');
+const http = require('http');
+const url = new URL("http://localhost:5000") ;
+url.pathname = '/bidule';
+
 http.createServer(function (request, response){
     response.writeHead(200);
-    response.write('<h1>Nous sommes en évaluation :D</h1>\n');
+    response.write('<h1>bidule</h1>\n');
     response.end();}).listen(5000);
-    console.log('Server running at ​http://localhost:5000​');
+    console.log(url.toString()); 
