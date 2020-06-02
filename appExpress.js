735b73639b58af33a49e44a690d9f0e5eeecf4d6
kumatata <coumba.camara75@gmail.com>
2020-06-02 17:59:42 +0200

    exo7

--- appExpress.js
+++ appExpress.js
@@ -2,6 +2,11 @@ var express = require('express');
 var http = require('http');
 var app = express();
 
+app.use((req,res,next) => {
+    console.log(req.protocol + '://' + req.get('host') + req.originalUrl) ;
+    next();
+      })
+
 app.get('/', function(req, res){
     res.writeHead(200);
     res.write('Hello Express!!!');
